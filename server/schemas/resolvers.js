ee4cd45ee1727e25b3e9c03c9e48f5db4b7203c1
Cody Cooper <cody_codster@live.com>
2023-03-18 15:43:48 -0500

    Updated resolver file with Query, getSingleUser

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,3 +1,22 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
 const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        //Get current users
+        getSingleUser: async (parent, args, context) => {
+            if(context.user) {
+                return User.findOne({
+                    $or: [{_id: context.user._id}, {username: context.user.username}]
+                })
+            };
+            throw new AuthenticationError('Must be logged in to view saved books!');
+        },
+    },
+    Mutation: {
+
+    },
+};
+
+module.exports = resolvers;
